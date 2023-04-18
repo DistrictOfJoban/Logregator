f230d5692c927a4a2f8ce2edc29a0cec537512dc
LX862 <kenny.mh.hui@outlook.com>
2023-04-19 01:43:20 +0800

    Drop 1.17/1.18 support

--- src/main/java/com/lx/logregator/Mappings.java
+++ src/main/java/com/lx/logregator/Mappings.java
@@ -9,18 +9,10 @@ import java.util.function.Consumer;
 
 public class Mappings {
     public static MutableText literalText(String content) {
-        #if MC_VERSION >= "11900"
-            return Text.literal(content);
-        #else
-            return new net.minecraft.text.LiteralText(content);
-        #endif
+        return Text.literal(content);
     }
 
     public static void registerCommand(Consumer<CommandDispatcher<ServerCommandSource>> callback) {
-        #if MC_VERSION < "11900"
-            net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> callback.accept(dispatcher));
-        #elif MC_VERSION >= "11900"
             net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback.EVENT.register((dispatcher, dedicated, third) -> callback.accept(dispatcher));
-        #endif
     }
 }
